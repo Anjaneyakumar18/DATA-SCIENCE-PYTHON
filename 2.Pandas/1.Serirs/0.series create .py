0f6a895399890f484109243a280015d124e8523b
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-10-13 12:58:28 +0530

    Delete 2.Pandas/1.Serirs directory

--- 2.Pandas/1.Serirs/0.series create .py
+++
@@ -1,6 +0,0 @@
-# Creation of a pandas series
-import pandas as pd
-
-values=[545,"ANJANEYA KUMAR","CSE-A","3ed-Year"]
-s=pd.Series(values)
-print(s)
--- 2.Pandas/1.Serirs/2.Data frames/0.create a data frame.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data={"Alphabet":[chr(65+i) for i in range(26)],"number":[i for i in range(1,27)]}
-
-df=pd.DataFrame(data)
-
-print(df)
